3ffc74189b05b81992884fea161233b87af02589
scad89 <scad200@gmail.com>
2022-04-15 01:17:04 +0300

    created signals, tasks, periodic tasks, add logic about time

--- notification_service/main/apps.py
+++ notification_service/main/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.db.models.signals import pre_save
 
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        from main import signals
