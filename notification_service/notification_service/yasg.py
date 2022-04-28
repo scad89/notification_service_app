471ce4d246472815cee32da100ba5869314be243
scad89 <scad200@gmail.com>
2022-04-28 12:09:34 +0300

    changes name in yasg

--- notification_service/notification_service/yasg.py
+++ notification_service/notification_service/yasg.py
@@ -6,7 +6,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Notification Service",
+        title="Notification Service App",
         default_version='v1',
         description="Test description",
         license=openapi.License(name="BSD License"),
